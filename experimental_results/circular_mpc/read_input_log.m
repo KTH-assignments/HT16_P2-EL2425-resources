368157a235f74c798b448f60f345aea524b374e6
alexandros filotheou <alefil@kth.se>
2016-12-18 20:24:31 +0100

    Adds inputs plots

--- experimental_results/circular_mpc/read_input_log.m
+++ experimental_results/circular_mpc/read_input_log.m
@@ -1,12 +1,10 @@
 clear all
 close all
-st = 1;
-en = 150;
+st = 150;
+en = 380;
 M = csvread('circular_mpc_inputs_log.csv');
 
-axis equal
-
 figure
-plot(M(1:150)*180/pi)
+plot(M(st:en)*180/pi)
 grid
 
