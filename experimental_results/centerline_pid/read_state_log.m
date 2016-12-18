fc0e02a8041540dd5954566924f792d74cd5dde8
alexandros filotheou <alefil@kth.se>
2016-12-18 21:21:59 +0100

    Adds experimental results for centerline_pid

--- experimental_results/centerline_pid/read_state_log.m
+++ experimental_results/centerline_pid/read_state_log.m
@@ -5,11 +5,14 @@ S = csvread('centerline_pid_states_log.csv');
 U = csvread('centerline_pid_inputs_log.csv');
 
 figure
-plot(S(1:450,1)*180/pi)
+plot(S(1:400,1)*180/pi)
+% axis([0 450 -30 20])
 grid
 
 figure
-plot(U(1:450)*180/pi)
+plot(U(1:400)*180/pi)
+% axis([0 450 -70 70])
+
 grid
 
 
