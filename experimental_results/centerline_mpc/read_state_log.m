ccb17fae33581f987f9477c4ef7c29c48748bc7d
alexandros filotheou <alefil@kth.se>
2016-12-21 16:54:27 +0100

    Adds experimental results from fixed centerline mpc

--- experimental_results/centerline_mpc/read_state_log.m
+++ experimental_results/centerline_mpc/read_state_log.m
@@ -6,16 +6,19 @@ U = csvread('centerline_mpc_inputs_log.csv');
 
 figure
 plot(S(1:end,1))
-axis([0 500 -1 1])
+axis([200 510 -2 2])
 grid
 
 figure
-plot(S(1:490,2)*180/pi)
+plot(S(:,2)*180/pi)
+axis([200 505 -180 50])
 grid
 
 figure
-plot(U(1:70)*180/pi)
+plot(U*180/pi)
 grid
+axis([34 80 -20 50])
+
 
 
 
